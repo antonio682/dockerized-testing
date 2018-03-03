3e6c9a5679e5db570ccb0b481ad1d33d9f4af1f5
Francisco Antonio Fernández Castaño <antonio@MacBook-Pro-de-Francisco.local>
2018-03-03 16:56:03 +0100

    Firt commit

---
+++ src/main/java/com/testator/dockerizedtesting/DockerizedTestingApplication.java
@@ -0,0 +1,12 @@
+package com.testator.dockerizedtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizedTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizedTestingApplication.class, args);
+	}
+}
